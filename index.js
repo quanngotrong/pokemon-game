bcdf79666e39c1c0d282ca0fb4c485603ec7bcf5
Quan Ngo <ngotrongquan116@gmail.com>
2026-03-18 23:53:00 +0700

    feat: add moving character

--- index.js
+++ index.js
@@ -10,6 +10,109 @@ c.fillRect(0, 0, canvas.width, canvas.height);
 
 const image = new Image();
 image.src = './img/Pellet Town.png';
-image.onload = () => {
-    c.drawImage(image, -750, -550);
+
+
+const playerImage = new Image();
+playerImage.src = './img/playerDown.png';
+
+class Sprite {
+    constructor({ 
+        position, 
+        velocity, 
+        image 
+    }) {
+        this.position = position;
+        this.image = image;
+    }
+
+    draw() {
+        c.drawImage(this.image, this.position.x, this.position.y);
+    }
 }
+
+const background = new Sprite({
+    position: {
+        x: -755,
+        y: -650
+    },
+    image: image
+});
+
+const keys = {
+    w: {
+        pressed: false
+    },
+    a: {
+        pressed: false
+    },
+    s: {
+        pressed: false
+    },
+    d: {
+        pressed: false
+    }
+};
+
+function animate() {
+    requestAnimationFrame(animate);
+
+    background.draw();
+
+    c.drawImage(
+        playerImage,
+        0,
+        0,
+        playerImage.width / 4,
+        playerImage.height,
+        canvas.width / 2 - (playerImage.width / 4) / 2,
+        canvas.height / 2 - playerImage.height / 2,
+        playerImage.width / 4,
+        playerImage.height
+    );
+
+    if (keys.w.pressed) {
+        background.position.y += 3;
+    } else if (keys.a.pressed) {
+        background.position.x += 3;
+    } else if (keys.s.pressed) {
+        background.position.y -= 3;
+    } else if (keys.d.pressed) {
+        background.position.x -= 3;
+    }
+}
+
+animate();
+
+window.addEventListener('keydown', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = true;
+            break;
+        case 'a':
+            keys.a.pressed = true;
+            break;
+        case 's':
+            keys.s.pressed = true;
+            break;
+        case 'd':
+            keys.d.pressed = true;
+            break;
+    }
+});
+
+window.addEventListener('keyup', (e) => {
+    switch (e.key) {
+        case 'w':
+            keys.w.pressed = false;
+            break;
+        case 'a':
+            keys.a.pressed = false;
+            break;
+        case 's':
+            keys.s.pressed = false;
+            break;
+        case 'd':
+            keys.d.pressed = false;
+            break;
+    }
+});
